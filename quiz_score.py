385a90b61446dd96a11b8067226395c0c28800b7
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-09-25 13:01:37 -0400

    Create quiz_score.py

---
+++ quiz_score.py
@@ -0,0 +1,15 @@
+# Ethan Lawrence
+# September 25 2024
+# Quiz score
+
+# Input
+fname = input('Please input your first name:    ')
+score1 = float(input('Please enter the First quiz score:    '))
+score2 = float(input('Please enter the Second quiz score:    '))
+score3 = float(input('Please enter the Third quiz score:    '))
+
+# Process
+average_score = (score1 + score2 + score3) / 3
+
+# Output
+print (f'(Hello {fname} Your average score is {average_score}')
